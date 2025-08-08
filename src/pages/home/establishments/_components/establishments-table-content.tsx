c1a621abc91a09431a0a0d696067f447b2efd74d
Yan-CarlosIF <yan312005@gmail.com>
2025-08-08 15:07:58 -0300

    feat: added listing in establishments page

--- src/pages/home/establishments/_components/establishments-table-content.tsx
+++ src/pages/home/establishments/_components/establishments-table-content.tsx
@@ -2,7 +2,10 @@ import { Button, Flex, Text, useDisclosure } from "@chakra-ui/react";
 import { Plus } from "lucide-react";
 import { useContext } from "react";
 
+import { ActionBar } from "@/components/ActionBar/action-bar";
+import { DeleteAlertDialog } from "@/components/ActionBar/delete-alert-dialog";
 import { Pagination } from "@/components/Pagination/pagination";
+import { TableCheckboxContext } from "@/context/checkboxContext";
 import { userContext } from "@/context/userContext";
 
 import { AddEstablishmentModal } from "./EstablishmentModal";
@@ -17,8 +20,13 @@ export function EstablishmentsTableContent({
   page,
 }: EstablishmentsTableContentProps) {
   const { user, isLoading } = useContext(userContext);
-
+  const { selectedData, setSelectedData } = useContext(TableCheckboxContext);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const {
+    isOpen: isCancelOpen,
+    onOpen: onOpenCancel,
+    onClose: onCloseCancel,
+  } = useDisclosure();
 
   return (
     <Flex px="40px" mt="40px" align="center" justify="space-between">
@@ -46,6 +54,38 @@ export function EstablishmentsTableContent({
       )}
 
       <Pagination w="fit-content" currentPage={page} lastPage={lastPage} />
+      <ActionBar count={selectedData.length}>
+        <Button
+          color="gray_800"
+          variant="outline"
+          size="sm"
+          onClick={() => setSelectedData([])}
+        >
+          Cancelar
+        </Button>
+        {selectedData?.length === 1 && (
+          <>
+            <Button
+              color="gray_800"
+              variant="outline"
+              size="sm"
+              onClick={onOpen}
+            >
+              Editar
+            </Button>
+            <AddEstablishmentModal isOpen={isOpen} onClose={onClose} />
+          </>
+        )}
+        <Button colorScheme="red" size="sm" onClick={onOpenCancel}>
+          Excluir
+        </Button>
+        <DeleteAlertDialog
+          type="book"
+          data={selectedData.map((book) => book.id)}
+          isOpen={isCancelOpen}
+          onClose={onCloseCancel}
+        />
+      </ActionBar>
     </Flex>
   );
 }
--- src/pages/home/establishments/index.tsx
+++ src/pages/home/establishments/index.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/context/checkboxContext";
 import { userContext } from "@/context/userContext";
 import { NextPageWithLayout } from "@/pages/_app";
+import { useListEstablishments } from "@/services/Establishments/useListEstablishments";
 import { withAuthServerSideProps } from "@/utils/withAuth";
 
 import { EstablishmentsTableContent } from "./_components/establishments-table-content";
@@ -19,109 +20,6 @@ interface EstablishmentsPageProps {
   page: number;
 }
 
-const dummyData = [
-  {
-    id: "1",
-    name: "Livraria Central",
-    cnpj: "12.345.678/0001-90",
-    city: "Fortaleza",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "2",
-    name: "Estante Cultural",
-    cnpj: "98.765.432/0001-12",
-    city: "Aracati",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "3",
-    name: "Livraria Central",
-    cnpj: "12.345.678/0001-90",
-    city: "Fortaleza",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "4",
-    name: "Estante Cultural",
-    cnpj: "98.765.432/0001-12",
-    city: "Aracati",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "5",
-    name: "Livraria Central",
-    cnpj: "12.345.678/0001-90",
-    city: "Fortaleza",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "6",
-    name: "Estante Cultural",
-    cnpj: "98.765.432/0001-12",
-    city: "Aracati",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "7",
-    name: "Livraria Central",
-    cnpj: "12.345.678/0001-90",
-    city: "Fortaleza",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "8",
-    name: "Estante Cultural",
-    cnpj: "98.765.432/0001-12",
-    city: "Aracati",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "9",
-    name: "Livraria Central",
-    cnpj: "12.345.678/0001-90",
-    city: "Fortaleza",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-  {
-    id: "10",
-    name: "Estante Cultural",
-    cnpj: "98.765.432/0001-12",
-    city: "Aracati",
-    state: "CE",
-    description: "Descrição do estabelecimento",
-    cep: "12345-678",
-    district: "Centro",
-  },
-];
-
 const EstablishmentsPageContent: NextPageWithLayout<
   EstablishmentsPageProps
 > = ({ page }) => {
@@ -129,11 +27,13 @@ const EstablishmentsPageContent: NextPageWithLayout<
   const isAdmin = !isLoading && user?.permission === "admin";
 
   const [search, setSearch] = useState("");
-  const [establishments, setEstablishments] = useState(dummyData);
 
   const { selectedData, toggleSelectAll } = useContext(TableCheckboxContext);
 
-  const filteredEstablishments = establishments.filter((e) => {
+  const { data, isLoading: isEstablishmentsLoading } =
+    useListEstablishments(page);
+
+  const filteredEstablishments = data?.establishments.filter((e) => {
     return (
       e.name.toLowerCase().includes(search) ||
       e.cnpj.toLowerCase().includes(search)
@@ -147,30 +47,36 @@ const EstablishmentsPageContent: NextPageWithLayout<
         placeholder="Buscar pelo nome ou CNPJ"
         searchValue={search}
       />
-      <BaseTable
-        h="612px"
-        checkbox={isAdmin}
-        isCheckboxChecked={
-          selectedData?.length === filteredEstablishments?.length
-        }
-        isCheckboxIndeterminate={
-          selectedData?.length > 0 &&
-          selectedData?.length < filteredEstablishments?.length
-        }
-        onCheckboxChange={() => toggleSelectAll(filteredEstablishments)}
-        headers={
-          <>
-            <Th>Nome</Th>
-            <Th>CNPJ</Th>
-            <Th>Endereço</Th>
-          </>
-        }
-      >
-        {filteredEstablishments.map((e) => (
-          <EstablishmentsTableItem key={e.id} establishment={e} />
-        ))}
-      </BaseTable>
-      <EstablishmentsTableContent page={page} lastPage={page} />
+      {isEstablishmentsLoading ? (
+        <div>Carregando...</div>
+      ) : (
+        <>
+          <BaseTable
+            h="612px"
+            checkbox={isAdmin}
+            isCheckboxChecked={
+              selectedData?.length === filteredEstablishments?.length
+            }
+            isCheckboxIndeterminate={
+              selectedData?.length > 0 &&
+              selectedData?.length < filteredEstablishments?.length
+            }
+            onCheckboxChange={() => toggleSelectAll(filteredEstablishments)}
+            headers={
+              <>
+                <Th>Nome</Th>
+                <Th>CNPJ</Th>
+                <Th>Endereço</Th>
+              </>
+            }
+          >
+            {filteredEstablishments.map((e) => (
+              <EstablishmentsTableItem key={e.id} establishment={e} />
+            ))}
+          </BaseTable>
+          <EstablishmentsTableContent page={page} lastPage={data?.lastPage} />
+        </>
+      )}
     </>
   );
 };
